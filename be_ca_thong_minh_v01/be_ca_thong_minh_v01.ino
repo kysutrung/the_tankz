d3d72f48279523b3ee72da873a0128609f3040a5
trung_pc_l <trungnd2767@gmail.com>
2024-10-31 22:22:38 +0700

    add a few function

--- be_ca_thong_minh_v01/be_ca_thong_minh_v01.ino
+++ be_ca_thong_minh_v01/be_ca_thong_minh_v01.ino
@@ -8,29 +8,91 @@
 //4 Code cho servo sg90
 //5 code cho sưởi
 
-//Màn này chạy với điện 5V, 3v3 bị mờ
-// Địa chỉ I2C của LCD, thường là 0x27 hoặc 0x3F
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+//Khai báo chân cắm các linh kiện
+const int BUTTON_01 = 34; //Nút chuyển chế độ
+const int BUTTON_02 = 35; //Nút điều chỉnh thông số trong chế độ
+LiquidCrystal_I2C lcd(0x27, 16, 2); //Nếu không chạy thì thử 0x27 hoặc 0x3F
 
 //Các biến toàn cục
 int SET_WATER_TEMP = 25;
+int WATER_TEMP = 0;
 int FEED_TIME = 12;
 int FIL_TIME = 6;
+bool IS_FILTER_RUN = 0;
+bool IS_HEATING_RUN = 0;
+int MENU_MODE = 0;
+bool HAVE_ACCESS_THIS_MODE = 0;
+bool STILL_PRESS_BUT_1 = 0;`
+bool STILL_PRESS_BUT_2 = 0;
 
-int WATER_TEMP;
-bool isFilterRun = 0;
-bool isHeatingRun = 0;
 
 //Khai báo các hàm
+void getButton(){
+  int but1State = digitalRead(BUTTON_01);
+  int but2State = digitalRead(BUTTON_02);
+
+  if (but1State == 0 && STILL_PRESS_BUT_1 == 0){
+    MENU_MODE++;
+    STILL_PRESS_BUT_1 = 1;
+  }
+  if (but1State == 1 && STILL_PRESS_BUT_1 == 1){
+    STILL_PRESS_BUT_1 = 0;
+  }
+  if(MENU_MODE > 3){
+    MENU_MODE = 0;
+  }
+
+  if (but2State == 0 && STILL_PRESS_BUT_2 == 0){
+    HAVE_ACCESS_THIS_MODE++;
+    STILL_PRESS_BUT_2 = 1;
+  }
+  if (but2State == 1 && STILL_PRESS_BUT_2 == 1){
+    STILL_PRESS_BUT_2 = 0;
+  }
+  if(HAVE_ACCESS_THIS_MODE > 1){
+    HAVE_ACCESS_THIS_MODE = 0;
+  }
+}
+
+void menuInteraction(){
+  
+}
+
 void setMainScreen(){
   lcd.clear();
   lcd.setCursor(0, 0);  // Cột + dòng
   lcd.print("T   F   H   Feed");
+
+
+  //Hiển thị nhiệt độ nước
   lcd.setCursor(0, 1);
-  lcd.print("26  OFF OFF 6H");
+  lcd.print(String(WATER_TEMP));
+
+  //Hiển thị trạng thái lọc nước
+  lcd.setCursor(4, 1);
+  if(IS_FILTER_RUN){
+    lcd.print("ON");
+  }
+  else{
+    lcd.print("OFF");
+  }
+
+  //Hiển thị trạng thái sưởi nước
+  lcd.setCursor(8, 1);
+  if(IS_HEATING_RUN){
+    lcd.print("ON");
+  }
+  else{
+    lcd.print("OFF");
+  }
+
+  //Hiển thị khoảng thời gian giữa các lần cho ăn
+  lcd.setCursor(12, 1);
+  lcd.print(String(FEED_TIME) + "H")
+  }
 }
 
-void setHeating(){
+void setHeatingTime(){
   lcd.clear();
   lcd.setCursor(0, 0);  // Cột + dòng
   lcd.print("Heating Setup:");
@@ -54,14 +116,13 @@ void setFeedingTime(){
   lcd.print("Every " + String(FEED_TIME) + "H");
 }
 
-void setup() {
+void setup(){
   Wire.begin(21, 22);  // Khởi tạo I2C trên ESP32 (GPIO21 -> SDA, GPIO22 -> SCL)
-  lcd.init();          // Khởi động LCD
-  lcd.backlight();     // Bật đèn nền LCD
+  lcd.init();
+  lcd.backlight();
 
-  setHeating();
 }
 
-void loop() {
+void loop(){
 
 }
