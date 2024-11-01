32875aaf27c3801efd55fc05786e2c2d82352b74
trung_pc_l <trungnd2767@gmail.com>
2024-11-01 23:35:29 +0700

    add servo code

--- be_ca_thong_minh_v01/be_ca_thong_minh_v01.ino
+++ be_ca_thong_minh_v01/be_ca_thong_minh_v01.ino
@@ -1,16 +1,25 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP32Servo.h>
 
 //Cần làm gì tiếp
-//1 Code đọc nhiệt độ từ cảm biến nước
+//1 Code đọc nhiệt độ từ cảm biến nước 
 //2 Code điều bật tắt relay lọc nước
-//3 Code cho nút bấm
 //4 Code cho servo sg90
 //5 code cho sưởi
 
 //Khai báo chân cắm các linh kiện
+
+const int SERVO_PIN = 18;
 const int BUTTON_01 = 13; //Nút chuyển chế độ
 const int BUTTON_02 = 12; //Nút điều chỉnh thông số trong chế độ
+const int ONE_WIRE_BUS = 2; //Chân tín hiệu của cảm biến nhiệt độ nước
+OneWire oneWire(ONE_WIRE_BUS);
+Servo myServo;
+DallasTemperature sensors(&oneWire);
+
 LiquidCrystal_I2C lcd(0x27, 16, 2); //Nếu không chạy thì thử 0x27 hoặc 0x3F
 
 //Các biến toàn cục
@@ -21,7 +30,6 @@ int FIL_TIME = 6;
 bool IS_FILTER_RUN = 0;
 bool IS_HEATING_RUN = 0;
 int MENU_MODE = 0;
-int LAST_MENU_MODE = 0;
 int HAVE_ACCESS_THIS_MODE = 0;
 bool STILL_PRESS_BUT_1 = 0;
 bool STILL_PRESS_BUT_2 = 0;
@@ -58,8 +66,30 @@ void getButton(){
 
 }
 
+void getWaterTemp(){
+  sensors.requestTemperatures(); 
+  WATER_TEMP = sensors.getTempCByIndex(0);
+}
+
+void feedTheFishs(){
+  myServo.write(180);
+  delay(740);
+  myServo.write(90);
+}
+
 void menuInteraction(){
-  
+  if(MENU_MODE == 0 && HAVE_ACCESS_THIS_MODE == 1){
+    setMainScreen();
+  }
+  if(MENU_MODE == 1 && HAVE_ACCESS_THIS_MODE == 1){
+    setHeating();
+  }
+  if(MENU_MODE == 2 && HAVE_ACCESS_THIS_MODE == 1){
+    setFilterTime();
+  }
+  if(MENU_MODE == 3 && HAVE_ACCESS_THIS_MODE == 1){
+    setFeedingTime();
+  }
 }
 
 void setMainScreen(){
@@ -96,7 +126,7 @@ void setMainScreen(){
   
 }
 
-void setHeatingTime(){
+void setHeating(){
   lcd.clear();
   lcd.setCursor(0, 0);  // Cột + dòng
   lcd.print("Heating Setup:");
@@ -123,6 +153,8 @@ void setFeedingTime(){
 void setup(){
   pinMode(BUTTON_01, INPUT_PULLUP);
   pinMode(BUTTON_02, INPUT_PULLUP);
+  sensors.begin();
+  myServo.attach(SERVO_PIN);
   Wire.begin(21, 22);  // Khởi tạo I2C trên ESP32 (GPIO21 -> SDA, GPIO22 -> SCL)
   lcd.init();
   lcd.backlight();
